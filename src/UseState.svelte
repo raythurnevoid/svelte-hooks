7087569eb0ca3f0a117b23fd34732bcd40efc8ae
Ray Thurne Void <raythurnevoid@gmail.com>
2020-12-29 00:46:29 +0100

    First implementation

---
+++ src/UseState.svelte
@@ -0,0 +1,47 @@
+<script context="module" lang="ts">
+	let count = 0;
+</script>
+
+<script lang="ts">
+	import Use from "./Use.svelte";
+	import { onMount, tick } from "svelte";
+
+	export let value: any;
+	export let onUpdate: (oldValue?: any) => void;
+	export const id: string = `../../../../packages/common/hooks/UseState-${count++}`;
+
+	let valueMemo: {value: any};
+	onMount(() => {
+		tick().then(() => {
+			valueMemo = {value};
+		});
+	});
+
+	function onValueUpdate(value: any) {
+		if (valueMemo === undefined) return;
+
+		if (value !== valueMemo.value) {
+			const currentValueMemo = valueMemo.value;
+
+			onUpdate(valueMemo.value);
+
+			if (currentValueMemo === valueMemo.value) {
+				// Prevent infinite loop when setValue is called during onUpdate
+				valueMemo.value = value;
+			}
+		}
+	}
+
+	export function setValue(newValue: typeof value) {
+		if (valueMemo) valueMemo.value = newValue;
+		value = newValue;
+	}
+
+	export function getValue() {
+		return value;
+	}
+</script>
+
+<svelte:options immutable={true} />
+
+<Use effect hook={() => onValueUpdate(value)} />
